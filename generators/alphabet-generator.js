b6813e98bcfdc2c661ac7eb500f695b24fad536c
Raine <raineorshine@gmail.com>
2012-04-17 00:36:41 -0600

    Final alphabet generator.

--- generators/alphabet-generator.js
+++ generators/alphabet-generator.js
@@ -1,6 +1,6 @@
-var RJS = require("../lib/RJS.2.5-node.js");
+var RJS = require("../lib/rjs.2.5.js");
 require("../lib/functional.js");
-require("../lib/RJS.2.3-node-proto.js");
+require("../lib/rjs.2.3-node-proto.js");
 
 /**********************************
  * Constants
@@ -8,22 +8,8 @@ require("../lib/RJS.2.3-node-proto.js");
 var TAB_SIZE = 2;
 
 /**********************************
- * Helper Functions
- **********************************/
-
-// functional's compose doesn't work in NodeJS
-var compose = function(f,g) {
-	return function() {
-		return f(g.apply(null, arguments));
-	};
-}
-var add = function(x,y) { return x + y; };
-
-/**********************************
  * Build Alphabet file
  **********************************/
-
-var letters = (26).map(compose(String.fromCharCode, add.curry(97)));
 
 // @param args { letter, tabLevel, max, body }
 var makeFor = function(args) {
@@ -40,11 +26,6 @@ var makeForRecur = function(index) {
 		body: index < 25 ? makeForRecur(index + 1) : ""
 	});
 };
-
-// do a reduction on letters: 
-//		"for...", "a", ["b", "c", "d", ...]
-//		"for...", "b", ["c", "d", "e", ...]
-//		"for...", "c", ["c", "d", "f", ...]
 
 var output = makeForRecur(0);
 
